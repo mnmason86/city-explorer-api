ff4ce0b0c5582cc775b17280e047cd0be32a28c9
Mandy Mason <mnmason86@gmail.com>
2022-07-20 00:27:08 -0400

    added GET request for /weather, Forecast class, does not return city results to browser

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 'use strict';
+
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -7,12 +8,51 @@ const server = express();
 server.use(cors());
 const PORT = process.env.PORT;
 
-server.get('/hello', (request, response) => {
-  console.log(request);
-  response.send('hellO there');
-});
+
+class Forecast {
+  constructor(date,description) {
+    this.date = date;
+    this.description = description;
+  }
+
+  static getForecast(city) {
+    const dailyForecast = city.data.map((element) => {
+      const date = element.valid_date;
+      const description = `Daily Low: ${element.low_temp}, ` + `Daily High: ${element.high_temp} ` + `with ${element.weatherData.description}`;
+      return new Forecast (date, description);
+    });
+    return dailyForecast;
+  }
+}
+
+function locate (name, lat, lon, weatherInfo) {
+  try {
+    const city = weatherInfo.find((element) => {
+      return (
+        name === element.city_name &&
+        Math.floor(lat) === Math.floor(element.lat) &&
+        Math.floor(lon) === Math.floor(element.lon)
+      );
+    });
+    return city;
+  } catch (error) {
+    throw new Error('No data exists for this city' + error);
+  }
+}
+
+// /weather API endpoint
+server.get('/weather', (request, response) => {
+
+  const cityData = locate(request.query.city_name, request.query.lat, request.query.lon, weatherData);
+  console.log(cityData);
+  if (cityData) {
+    response.send('City Located');
+  } else {
+    response.status(404).send('No weather data exists for this city');
+  }
+  });
 
 
-server.listen(3000, () => {
-  console.log(PORT);
+server.listen(PORT, () => {
+  console.log('Server is running on port :: ' + PORT);
 });
